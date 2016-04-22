b87399476cbdbcc6c8b24ecca840954179404b0c
Chao <chaoguo94@gmail.com>
2016-04-22 16:49:21 -0700

    Update ROF_model.m

--- ROF_model.m
+++ ROF_model.m
@@ -2,18 +2,28 @@
 % We discretize the model using the method presented in the following
 % http://www.math.ucla.edu/~lvese/285j.1.05s/ROFScheme.pdf
 
+
+% PREPROCESSING
 % Load image in BMP format
-u = imread('Lena.bmp'); % Do not use double quote
-u = double(u); % Convert to double so you can add noise later
-% u = double(u);
-[M, N] =size(u);
+img = imread('Lena.bmp'); % Do not use double quote
+img = double(img); % Convert to double so you can add noise later
+[M, N] =size(img);
 
 % Display image with scaled colors
-imagesc(u);
+imagesc(img);
 axis image;
 axis off;
 colormap(gray);
 
 % Generate noise
-noise = randn(size(u));
-u0 = u + 30*noise; % Multiply by 1 the noise is too small to notice
+noise = rand(size(img));
+u0 = img + 20*noise; % Multiply by 1 the noise is too small to notice
+
+
+% NOISE REDUCTION
+% Implement the boundary condition
+u = padarray(img,[1 1]);
+u(1,2:size(u,2)-1) = img(1,:); % First row
+u(size(u,1),2:size(u,2)-1) = img(size(img,1),:); % Last row
+u(2:size(u,1)-1,1) = img(:,1); % First column
+u(size(u,2),2:size(u,1)-1) = img(:,size(img,2)); % Last column
