3dbbf163de8c50969ee582149e58a7c513df2837
Chao <chaoguo@Chaos-MacBook-Air.local>
2016-05-05 22:31:36 -0700

    Added scheme

--- ROF_model.m
+++ ROF_model.m
@@ -15,10 +15,10 @@ img = double(img); % Convert to double so you can add noise later
 [M, N] =size(img);
 
 % Display image with scaled colors
-imagesc(img);
-axis image;
-axis off;
-colormap(gray);
+%imagesc(img);
+%axis image;
+%axis off;
+%colormap(gray);
 
 % Generate noise
 noise = rand(size(img));
@@ -37,3 +37,24 @@ u(size(u,1),2:size(u,2)-1) = u0(size(u0,1),:); % Last row
 for i=2:size(u)-1
     u(2:size(u)-1,i) = u0(:,i-1);
 end
+
+% Implement the scheme for interior points
+delta_t = 1;
+lambda = 1;
+h =1;
+c1 = 1;
+c2 = 1;
+c3 = 1;
+c4 = 1;
+
+for i=2:size(u)-1
+    for j=2:size(u)-1
+        u(i,j) =u(i+1,j) + u(i-1,j);% (1/(1 + delta_t + delta_t*(c1+c2+c3+c4)/2*lambda*h*h))*(u + delta_t*u + delta_t*(c1*u(i+1,j)+c2*u(i-1,j)+c3*u(i,j+1)+c4*u(i,j-1)));
+    end
+end
+% Display denoised image
+imagesc(u);
+axis image;
+axis off;
+colormap(gray);
+            
