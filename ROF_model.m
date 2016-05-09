7e9df96263b30c4d97df400a1dc828bc26495938
Chao <chaoguo94@gmail.com>
2016-05-09 15:13:26 -0700

    Update ROF_model.m
    fully implemented numercail scheme; replaced 0.001 with realim

--- ROF_model.m
+++ ROF_model.m
@@ -23,6 +23,11 @@ img = double(img); % Convert to double so you can add noise later
 noise = rand(size(img));
 f = img + 20*noise; % If multiply by 1 then the noise is too small to notice
 
+imagesc(f);
+axis image;
+axis off;
+colormap(gray);
+
 
 % NOISE REDUCTION
 % Implement the boundary condition
@@ -37,11 +42,14 @@ for i = 2:size(u)-1
     u(2:size(u)-1,i) = f(:,i-1);
 end
 
+% Pad f
+f = padarray(f,[1 1]);
+
 % Implement the scheme for interior points
 delta_t = 1;
-lambda = 1;
+lambda = 20;
 h = 1;
-e = 0.001;
+e = realim; % smallest float in Matlab
 
 for iteration = 1:100
     for i = 2:size(u)-1
@@ -50,7 +58,14 @@ for iteration = 1:100
             c2 = 1/sqrt(e^2 + ((u(i,j)-u(i-1,j))/h)^2 + ((u(i-1,j+1)-u(i-1,j))/h)^2);
             c3 = 1/sqrt(e^2 + ((u(i+1,j)-u(i,j))/h)^2 + ((u(i,j+1)-u(i,j))/h)^2);
             c4 = 1/sqrt(e^2 + ((u(i+1,j-1)-u(i,j-1))/h)^2 + ((u(i,j)-u(i,j-1))/h)^2);
-            u(i,j) = (1/(1+delta_t+delta_t*(c1+c2+c3+c4)/2*lambda*h^2))*(u(i,j)+delta_t*(c1*u(i+1,j)+c2*u(i-1,j)+c3*u(i,j+1)+c4*u(i,j-1))/2*lambda*h^2);
+            u(i,j) = (1/(1+delta_t+delta_t*(c1+c2+c3+c4)/2*lambda*h^2))*...
+                (u(i,j)+delta_t*f(i,j)+delta_t*(c1*u(i+1,j)+c2*u(i-1,j)+c3*u(i,j+1)+c4*u(i,j-1))/2*lambda*h^2);
         end
     end
 end
+
+figure; % open new window
+imagesc(u);
+axis image;
+axis off;
+colormap(gray);
