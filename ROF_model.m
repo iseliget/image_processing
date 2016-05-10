59013e0fb40941c3e3c0bd4cf0125dcb69bca0e1
Chao <chaoguo@wifi-131-179-42-246.host.ucla.edu>
2016-05-10 12:47:03 -0700

    pad f

--- ROF_model.m
+++ ROF_model.m
@@ -31,8 +31,7 @@ colormap(gray);
 
 % NOISE REDUCTION
 % Implement the boundary condition
-% u = padarray(img,[1 1]); 
-% If you don't have the function padarray then you might have to use loops
+% You can use padarray() if your Matlab has this function
 u = zeros(size(f)+2);
 u(2:size(u)-1,1) = f(:,1); % First column
 u(2:size(u)-1,size(u,2)) = f(:,size(f,2)); % Last column
@@ -42,12 +41,19 @@ for i = 2:size(u)-1
     u(2:size(u)-1,i) = f(:,i-1);
 end
 
-% Pad f
-f = padarray(f,[1 1]);
+% Pad f to let size(f) = size(u)
+f_pad = zeros(size(f)+2);
+f_pad(2:size(f_pad)-1,1) = f(:,1); % First column
+f_pad(2:size(f_pad)-1,size(f_pad,2)) = f(:,size(f,2)); % Last column
+f_pad(1,2:size(f_pad,2)-1) = f(1,:); % First row
+f_pad(size(f_pad,1),2:size(f_pad,2)-1) = f(size(f,1),:); % Last row
+for i = 2:size(f_pad)-1
+    f_pad(2:size(f_pad)-1,i) = f(:,i-1);
+end
 
 % Implement the scheme for interior points
 delta_t = 1;
-lambda = 15; % higher the lambda, smoother the image
+lambda = 15;
 h = 1;
 e = eps; 
 
@@ -63,12 +69,11 @@ for iteration = 1:num_of_iteration
             c2 = 1/sqrt(e^2 + ((u(i,j)-u(i-1,j))/h)^2 + ((u(i-1,j+1)-u(i-1,j))/h)^2);
             c3 = 1/sqrt(e^2 + ((u(i+1,j)-u(i,j))/h)^2 + ((u(i,j+1)-u(i,j))/h)^2);
             c4 = 1/sqrt(e^2 + ((u(i+1,j-1)-u(i,j-1))/h)^2 + ((u(i,j)-u(i,j-1))/h)^2);
-            u(i,j) = (1/(1+delta_t+delta_t*(c1+c2+c3+c4)/2*lambda*h^2))*...
-                (u(i,j)+delta_t*f(i,j)+delta_t*(c1*u(i+1,j)+c2*u(i-1,j)+c3*u(i,j+1)+c4*u(i,j-1))/2*lambda*h^2);
+            u(i,j) = (1/(1+delta_t+delta_t*(c1+c2+c3+c4)/2*lambda*h^2))*(u(i,j)+delta_t*f_pad(i,j)+delta_t*(c1*u(i+1,j)+c2*u(i-1,j)+c3*u(i,j+1)+c4*u(i,j-1))/2*lambda*h^2);
         end
     end
     % Compute L^2 norm
-    energy(c,1) = sum(sum(abs(f-u).^2,2),1);
+    %energy(c,1) = sum(sum(abs(f-u).^2,2),1);
 end
 
 figure; % open new window
